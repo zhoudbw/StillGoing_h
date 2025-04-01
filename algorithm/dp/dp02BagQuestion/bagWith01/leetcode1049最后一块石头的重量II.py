d568850cdde38402f6c5680f197da202fa3a1f88
zhoudbw <973121322@qq.com>
2025-04-01 20:24:55 +0800

    + 动态规划: leetcode1049最后一块石头的重量II;

--- algorithm/dp/dp02BagQuestion/bagWith01/leetcode1049最后一块石头的重量II.py
+++ algorithm/dp/dp02BagQuestion/bagWith01/leetcode1049最后一块石头的重量II.py
@@ -4,3 +4,35 @@ class Solution( object ):
         :type stones: List[int]
         :rtype: int
         """
+        
+        w = sum( stones ) // 2
+        n = len( stones )
+        dp = [ 0 ] * ( w + 1 )
+        for i in range( 0, n ):
+            for k in range( w, stones[ i ] - 1, -1 ):
+                dp[ k ] = max( dp[ k ], stones[ i ] + dp[ k - stones[ i ] ] )
+        leftSum = sum( stones ) - dp[ w ]
+        return abs( leftSum - dp[ w ] )
+
+
+    def lastStoneWeightII( self, stones ):
+        w = sum( stones ) // 2
+        n = len( stones )
+        dp = [ [ 0 ] * ( w + 1 ) for _ in range( n ) ]
+        for i in range( stones[ 0 ], w + 1 ): dp[ 0 ][ i ] = stones[ 0 ]
+        for i in range( 1, n ):
+            for k in range( 0, w + 1 ):
+                if k >= stones[ i ]:
+                    dp[ i ][ k ] = max( dp[ i - 1 ][ k ], stones[ i ] + dp[ i - 1 ][ k - stones[ i ] ] )
+                else:
+                    dp[ i ][ k ] = dp[ i - 1 ][ k ]
+        return ( sum( stones ) - dp[ n - 1 ][ w ] ) - dp[ n - 1 ][ w ]
+    
+    
+if __name__ == '__main__':
+    mStones = [ 2, 7, 4, 1, 8, 1 ]
+    print( Solution().lastStoneWeightII( mStones ) )
+    
+        
+        
+        
